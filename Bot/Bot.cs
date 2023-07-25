78b14a8d018e1c218f9681760bb79ea29b0f277d
xenderLink <heihachi1337mishima@gmail.com>
2023-07-25 14:43:26 +0500

    Лишняя строка

--- Bot/Bot.cs
+++ Bot/Bot.cs
@@ -19,7 +19,7 @@ namespace BotSpace;
 sealed class Bot
 {
     private TelegramBotClient? Client;
-    private readonly string token = "token";
+    private readonly string token = "6090888687:AAEIa6pU-9w0dmWrlTPsUdgcWA5ASZMvOUk";
     ReceiverOptions receiverOptions;
 
     InlineKeyboardMarkup inlineKeyboardMarkup;

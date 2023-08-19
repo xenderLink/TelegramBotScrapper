a6b7bde39ec78e40a404df874e98dea90463ed53
xenderLink <heihachi1337mishima@gmail.com>
2023-08-20 00:51:10 +0500

    s

--- TelegramBotScrapper/Bot/Bot.cs
+++ TelegramBotScrapper/Bot/Bot.cs
@@ -18,7 +18,7 @@ namespace BotSpace;
 sealed class Bot
 {
     private TelegramBotClient? Client;
-    private readonly string token = "6090888687:AAEoYrmu4hIaxSJDUL5FZQrwRBLFkdsS8aQ";
+    private readonly string token = "token";
     ReceiverOptions receiverOptions;
 
     InlineKeyboardMarkup inlineKeyboardMarkup;
